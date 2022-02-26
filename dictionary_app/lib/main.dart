8389ac6b8e41d1d6ace242ba61183978a90ad07d
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-02-27 00:03:46 +0500

    add dictioanary pr

--- dictionary_app/lib/main.dart
+++ dictionary_app/lib/main.dart
@@ -38,61 +38,118 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.blueGrey[700],
         title: Center(child: Text(widget.title)),
       ),
-      body: SafeArea(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none,
-
-                      // borderSide: BorderSide(color: Colors.transparent)
-                    ),
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintText: 'Enter Word here...',
-
-                    // fillColor: Colors.grey[100]),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: new InputDecoration(
+                icon: new Icon(Icons.text_fields, color: Colors.white),
+                hintText: 'Enter Word here...',
+                // border: InputBorder.none,
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.blueAccent, width: 32.0),
+                    borderRadius: BorderRadius.circular(10.0)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.yellow, padding: EdgeInsets.all(8)),
+                  onPressed: () {},
+                  child: new Icon(
+                    Icons.near_me,
                   ),
                 ),
-              ),
-
-
+              ],
             ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 1.0,
+              vertical: 30.0,
+            ),
+            child: Container(
+              height: 2.0,
+              // width: 10,
+              // width:1690.0,
+              color: Colors.yellow,
+            ),
+          ),
+          // const Divider(
+          //   color: Colors.yellow,
+          //   height: 25,
+          //   thickness: 2,
+          // ),
+          // ElevatedButton(
+          //   style: ElevatedButton.styleFrom(
+          //     primary: Colors.red, // background
+          //     onPrimary: Colors.white, // foreground
+          //   ),),
+          // ElevatedButton(onPressed: (){}, child: child)
 
+          // TextField(
+          //     decoration: InputDecoration(
+          //       border: OutlineInputBorder(
+          //         borderRadius: BorderRadius.circular(8),
+          //         borderSide: BorderSide.none,
+          //       ),
+          //       fillColor: Colors.white,
+          //       filled: true,
+          //       hintText: 'Enter Word here...',
+          //       // fillColor: Colors.grey[100]),
+          //     ),
+          //   ),
 
+          // Padding(
+          //   padding: const EdgeInsets.all(8.0),
+          //   child: Container(
+          //     child: TextField(
+          //       decoration: InputDecoration(
+          //         border: OutlineInputBorder(
+          //           borderRadius: BorderRadius.circular(8),
+          //           borderSide: BorderSide.none,
+          //         ),
+          //         fillColor: Colors.white,
+          //         filled: true,
+          //         hintText: 'Enter Word here...',
+          //         // fillColor: Colors.grey[100]),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          // IconButton(
+          //   icon: Icon(Icons.visibility),
+          //   onPressed: () {
+          //     print('object');
+          //   },
+          // ),
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
+          // Row(
+          //   mainAxisAlignment: MainAxisAlignment.end,
+          //   children: [
+          //     Padding(
+          //       padding: const EdgeInsets.only(right: 18, bottom: 1),
+          //       // child: IconButton(
+          //       //   icon: const Icon(Icons.send),
+          //       //   tooltip: 'send to',
+          //       //   onPressed: () {},
+          //       // ),
+          //     ),
+          //   ],
+          // ),
 
-                  Padding(
-                    padding: const EdgeInsets.only(right: 18,bottom: 1),
-                    child: IconButton(
-                      icon: const Icon(Icons.send),
-                      tooltip: 'send to',
-                      onPressed: () {
-
-                      },
-                    ),
-                  ),
-                ],
-              ),
-
-
-
-
-
-            // prefixIcon: Icon(Icons.search),
-            // border: InputBorder.none,
-          ],
-        ),
+          // prefixIcon: Icon(Icons.search),
+          // border: InputBorder.none,
+        ],
       ),
 
       // body: Center(
