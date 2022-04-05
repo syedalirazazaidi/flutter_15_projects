6accc7bac102e7072dd04b00c679e19994c49996
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-04-05 17:37:53 +0500

    add geolocator

--- weatherapp/lib/core/services/weather_services.dart
+++ weatherapp/lib/core/services/weather_services.dart
@@ -12,35 +12,58 @@ class WeatherService {
     if (response.statusCode != 200) {
       throw Exception('Error Fetching data from server');
     }
-    final  decodeData=jsonDecode(response.body);
-// print(decodeData);
-    final parseData = decodeData.isNotEmpty?decodeData:{};
-// print(parseData);
-    // final parseData = jsonDecode(response.body) ??'';
-    final temperature = parseData["main"]['temp']??'';
-    final humidity = parseData["main"]['humidity']??'';
-    final wind = parseData["wind"]['speed']??'';
-    final country=parseData["sys"]['country']??'';
-    final description=parseData['weather'][0]['description']??'';
-    final cityName=parseData["name"]??'';
+    final decodeData = jsonDecode(response.body);
 
+    final parseData = decodeData.isNotEmpty ? decodeData : {};
 
-    // print(parseData);
-    return DayWeather(country: country,humidity: humidity,wind: wind,temperature: temperature,description: description,cityName:cityName);
+    final temperature = parseData["main"]['temp'] ?? '';
+    final humidity = parseData["main"]['humidity'] ?? '';
+    final wind = parseData["wind"]['speed'] ?? '';
+    final country = parseData["sys"]['country'] ?? '';
+    final description = parseData['weather'][0]['description'] ?? '';
+    final cityName = parseData["name"] ?? '';
 
-
+    return DayWeather(
+        country: country,
+        humidity: humidity,
+        wind: wind,
+        temperature: temperature,
+        description: description,
+        cityName: cityName);
   }
-  void getLocation()async{
-    print('Fetching location in progress');
-    http.Response locationCity = await http.get(Uri.parse('${Configs.locationApi}?lat=${lat}&lon=${lon}&appid=${Configs.apiKey}'));
-print('get location completed');
-   // final response = await http.get(locationCity);
 
-   if (locationCity.statusCode != 200 || locationCity==null) {
-     throw Exception('Error Fetching location from server');
-   }
-    final  locationData=jsonDecode(locationCity.body);
-   print(locationData);
+  Future<DayWeather?> getLocation(latitude, longitude) async {
+    if (latitude != null && longitude != null) {
+      print('get location in process $latitude ,$longitude');
+
+      http.Response locationCity = await http.get(Uri.parse(
+          // 'https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${Configs.apiKey}'
+        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=9992d178cf439a1c5e213b04853970bc'
+      ));
+
+      if (locationCity.statusCode != 200 || locationCity == null) {
+        throw Exception('Error Fetching location from server');
+      }
+
+      final locationData = jsonDecode(locationCity.body);
+      // print('locationData $locationData');
+      final parseData = locationData.isNotEmpty ? locationData : {};
+      print('parseData $parseData');
+      final temperature = parseData["main"]['temp'] ?? '';
+      final humidity = parseData["main"]['humidity'] ?? '';
+      final wind = parseData["wind"]['speed'] ?? '';
+      final country = parseData["sys"]['country'] ?? '';
+      final description = parseData['weather'][0]['description'] ?? '';
+      final cityName = parseData["name"] ?? '';
+
+      return DayWeather(
+          country: country,
+          humidity: humidity,
+          wind: wind,
+          temperature: temperature,
+          description: description,
+          cityName: cityName);
+    }
   }
 }
-// =${city}&appid=${apiKey}
+
--- weatherapp/lib/ui/screens/weather_screen.dart
+++ weatherapp/lib/ui/screens/weather_screen.dart
@@ -6,6 +6,7 @@ import 'package:weatherapp/core/models/weather.dart';
 import 'package:weatherapp/core/services/weather_services.dart';
 import 'package:weatherapp/ui/widgets/card_weather.dart';
 import 'package:weatherapp/ui/widgets/custom_search.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'package:intl/intl.dart';
 import 'package:weatherapp/ui/widgets/time_widget.dart';
@@ -23,8 +24,61 @@ class _WeatherScreenState extends State<WeatherScreen> {
   var day = DateFormat.d().format(DateTime.now());
   var month = DateFormat.LLLL().format(DateTime.now());
 
+  // String _locality = '';
+  // String _weather = '';
+  // Future<Position> getPosition() async {
+  //   Position position = await Geolocator
+  //        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  //   return position;
+  // }
+  // Future<Placemark> getPlacemark(double latitude, double longitude) async {
+  //   List<Placemark> placemark = await Geolocator.
+  //       placemarkFromCoordinates(latitude, longitude);
+  //   return placemark[0];
+  // }
+  Position? currentPosition;
+  // _getCurrentLocation() {
+  //   Geolocator.getCurrentPosition(
+  //           desiredAccuracy: LocationAccuracy.best,
+  //           forceAndroidLocationManager: true)
+  //       .then((Position position) {
+  //     setState(() {
+  //       currentPosition = position;
+  //     });
+  //   }).catchError((e) {
+  //     print(e);
+  //   });
+  // }
+  Future<Position> _getCurrentLocation() async {
+    Position position = await Geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    return position;
+  }
+
+
   @override
-  void initState() {}
+  void initState() {
+    super.initState();
+    _getCurrentLocation().then((position){
+      print(position);
+       _weatherService.getLocation(position.latitude, position.longitude).then((data){
+         setState(() {
+           cityweather = data;
+         });
+         // print('$data =========================');
+       });
+    }
+    );
+
+    // if (currentPosition != null) {
+    //  _weatherService.getLocation(currentPosition?.latitude,currentPosition?.longitude);
+    //   _getCurrentLocation();
+
+    //
+    // }
+
+  }
+
   void toggleIsLoading() {
     setState(() {
       isLoading = !isLoading;
@@ -56,12 +110,16 @@ class _WeatherScreenState extends State<WeatherScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // if (currentPosition != null) {
+    //   print('${currentPosition?.latitude}currentposition');
+    //   "LAT: ${currentPosition?.latitude}, LNG: ${currentPosition?.longitude}";
+    // }
+
     //build function run every update
     // initState() this lifecycle event call only ones
     // didUpdateWidget() use this widget if you want to compare the new widget and the old one
     // deactivate() this method can be call some state is removed from the tree temporarily or permanently
     // dispose() this method can be call some state is removed from the tree temporarily
-
 
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
